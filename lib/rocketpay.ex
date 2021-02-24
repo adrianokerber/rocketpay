a38f1009588e33371ce13b67d8e4b4cec2ea873e
Adriano Kerber <kerberpro@gmail.com>
2021-02-23 22:03:30 -0300

    :sparkles: Add new migration for User table along with User schema and exposed a method on the Domain module that do all creation logic for the user on the DB

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -6,4 +6,7 @@ defmodule Rocketpay do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias Rocketpay.Users.Create, as: UserCreate
+
+  defdelegate create_user(params), to: UserCreate, as: :call
 end
