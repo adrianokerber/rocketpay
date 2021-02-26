e90efe486fe87a54fb9617a1b7d67e65f6c59052
Adriano Kerber <kerberpro@gmail.com>
2021-02-25 23:19:41 -0300

    :poop: It smells I know, but is a good revamp but has issues that requires fixes

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -7,10 +7,11 @@ defmodule Rocketpay do
   if it comes from the database, an external API or others.
   """
   alias Rocketpay.Users.Create, as: UserCreate
-  alias Rocketpay.Accounts.{Deposit, Withdraw}
+  alias Rocketpay.Accounts.{Deposit, Transaction, Withdraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate deposit(params), to: Deposit, as: :call
   defdelegate withdraw(params), to: Withdraw, as: :call
+  defdelegate transaction(params), to: Transaction, as: :call
 end
